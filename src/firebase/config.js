e2e774dcbc1a4947cd4246fbe0b92b1128389875
Gibran Rizvi <gibranrzv@gmail.com>
2023-05-11 05:55:22 +0400

    Deploying with config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAZ_pN_esa98fZ7wn7qb8OAqwbuxclgR4Q",
+  authDomain: "hackathon-knight.firebaseapp.com",
+  projectId: "hackathon-knight",
+  storageBucket: "hackathon-knight.appspot.com",
+  messagingSenderId: "372008279071",
+  appId: "1:372008279071:web:5abae3388246b10abfc92a",
+};
